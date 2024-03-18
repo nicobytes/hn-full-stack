9ae3228dfb6132e0c4c6fc43407faf92e7b598a7
Nicolas Molina Monroy <hi@nicobytes.com>
2024-03-18 02:06:22 -0400

    Refactor TasksService and update metadata in layout

--- api/src/tasks/tasks.service.ts
+++ api/src/tasks/tasks.service.ts
@@ -9,20 +9,23 @@ import { Story } from '@database/entities/story.entity';
 import { DeletedStories } from '@database/entities/deleted.entity';
 import config from '@src/config';
 
+interface Hit {
+  author: string;
+  title: string;
+  created_at: string;
+  updated_at: string;
+  story_id: number;
+  story_title: string;
+  story_url: string;
+  story_text: string;
+  comment_text: string;
+  url: string;
+  _tags: string[];
+}
+
 interface HNResponse {
   hitsPerPage: number;
-  hits: Array<{
-    author: string;
-    title: string;
-    created_at: string;
-    updated_at: string;
-    story_id: number;
-    story_title: string;
-    story_url: string;
-    story_text: string;
-    comment_text: string;
-    _tags: string[];
-  }>;
+  hits: Hit[];
 }
 
 @Injectable()
@@ -54,27 +57,37 @@ export class TasksService {
     const allBannedStories = await this.deletedStoriesRepo.find();
     const allBannedStoriesIds = allBannedStories.map((story) => story.storyId);
     const stories = data.hits
+      .filter((hit) => {
+        return (
+          (hit.story_title || hit.title) &&
+          !allBannedStoriesIds.includes(hit.story_id)
+        );
+      })
       .map((hit) => {
         return this.storyRepo.create({
           id: hit.story_id,
           title: hit.story_title ?? hit.title,
           text: hit.story_text ?? hit.comment_text,
-          url: hit.story_url,
+          url: this.getUrl(hit),
           author: hit.author,
           createdAt: new Date(hit.created_at),
           updatedAt: new Date(hit.updated_at),
         });
       })
-      .filter((story) => {
-        return !allBannedStoriesIds.includes(story.id);
-      })
-      .reduce((stories, story) => {
+      .reduce<Story[]>((stories, story) => {
         if (!stories.find((s) => s.id === story.id)) {
           stories.push(story);
         }
         return stories;
       }, []);
-    const results = await Promise.all(stories);
-    return this.storyRepo.save(results);
+    return this.storyRepo.save(stories);
+  }
+
+  private getUrl(hit: Hit) {
+    const url = hit.story_url ?? hit.url;
+    if (url) {
+      return url;
+    }
+    return `https://news.ycombinator.com/item?id=${hit.story_id}`;
   }
 }
